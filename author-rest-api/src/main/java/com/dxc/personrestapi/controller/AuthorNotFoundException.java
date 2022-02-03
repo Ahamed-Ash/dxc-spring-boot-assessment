84303b3d967bdadcfb344019c0428c1b83ca5196
Ahamed-Ashik <96856514+Ahamed-Ash@users.noreply.github.com>
2022-02-03 19:00:20 +0530

    Delete author-rest-api directory

--- author-rest-api/src/main/java/com/dxc/personrestapi/controller/AuthorNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.dxc.authorrestapi.controller;
-
-public class AuthorNotFoundException extends RuntimeException {
-
-    AuthorNotFoundException(Integer id){
-        super("could not find author" +id);
-    }
-    
-}
--- author-rest-api/src/main/java/com/dxc/personrestapi/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dxc.authorrestapi.repository;
-
-import com.dxc.authorrestapi.model.Author;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author,Integer>{
-    
-}
